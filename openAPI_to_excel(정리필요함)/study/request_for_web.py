2779ec2494879f47523c29e906f6ba7e3ce4d981
maninblack87 <maninblack87@naver.com>
2024-12-23 16:20:33 +0900

    20241223 4nd

--- openAPI_to_excel(정리필요함)/study/request_for_web.py
+++
@@ -1,8 +0,0 @@
-import urllib.request as request
-import json
-
-json_str = request.urlopen("http://api.github.com/repositories").read()
-output = json.loads(json_str)
-
-print(type(output))
-print(output)
